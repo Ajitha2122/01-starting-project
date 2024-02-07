4a528a85f3779dd93cb7afdb7828d17dac4840f7
Ajitha2122 <ajitha@apptino.com>
2024-02-07 17:43:00 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/investment-calculator-logo.png";
+export default function Header() {
+  return (
+    <Header id="header">
+      <img src={logo} alt="Logo showing a money bag" />
+      <h1>Inverstment Calculator</h1>
+    </Header>
+  );
+}
